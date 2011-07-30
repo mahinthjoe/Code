ca4f06c64dfc54dbfcd6aac7546d6bcb92bb4a72
Mahinth Christensen <mahinthjoe@gmail.com>
2011-07-30 12:54:09 +0530

    Added Copyright message with auto data to footer.php

--- Starkers/footer.php
+++ Starkers/footer.php
@@ -20,7 +20,7 @@
 
 			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
-
+Copyright &copy; <?php echo date('y')?>
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
