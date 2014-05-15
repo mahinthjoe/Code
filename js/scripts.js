b967c0180ad7205f48a26b2d41ca02d05526ccdd
Mahinth Joe Christensen <mahinthjoe@gmail.com>
2014-05-15 17:38:23 +0530

    Added JavaScripts in js/

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/* Recursively traverse the HTML DOM using the passed node as starting point */
+  function traverse(obj){
+       var strNode=ancestor(obj) + obj.nodeName.toString()+"\n";
+          for(var i=0;i<obj.childNodes.length;i++)
+                strNode+=traverse(obj.childNodes.item(i));
+             return(strNode);
+       function ancestor(obj){
+        if(obj.parentNode != null) 
+         return('>'+ ancestor(obj.parentNode));
+        else
+         return('');
+        }
+  }
