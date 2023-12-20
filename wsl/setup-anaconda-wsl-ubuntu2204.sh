ba904a6656e72d2756c628dcd9724a68d07a2f51
Mahinth Joe <252034+mahinthjoe@users.noreply.github.com>
2023-12-20 12:55:42 +0530

    Anaconda Install in WSL linux

---
+++ wsl/setup-anaconda-wsl-ubuntu2204.sh
@@ -0,0 +1,8 @@
+sudo apt -y update
+sudo apt upgrade
+mkdir ~/Downloads && cd ~/Downloads
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+# Accept License and enable environment
+bash ~/Downloads/Anaconda3-2023.09-0-Linux-x86_64.sh
+
+
