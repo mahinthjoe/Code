a08213226c52e7d4575a119f08c743e69bd85792
Joe <mahinthjoe@gmail.com>
2024-01-18 13:42:11 +0530

    Update fedoraSetup.sh

--- fedora/fedoraSetup.sh
+++ fedora/fedoraSetup.sh
@@ -55,6 +55,8 @@ sudo dnf -y install snapd # Installing snap on Fedora
 sudo ln -s /var/lib/snapd/snap /snap # To enable classic snap support,
 sudo snap install hello-world
 sudo snap install co-pilot-cli --edge --devmode
+sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/winehq.repo
+sudo dnf install winehq-stable
 sudo snap install --classic code # or code-insiders #Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
