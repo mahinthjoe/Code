8e2744a3595d818a6c8fdb483e60f3f7674b1c95
Joe <mahinthjoe@gmail.com>
2024-01-18 15:45:19 +0530

    Update fedoraSetup.sh

--- fedora/fedoraSetup.sh
+++ fedora/fedoraSetup.sh
@@ -57,6 +57,10 @@ sudo snap install hello-world
 sudo snap install co-pilot-cli --edge --devmode
 sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/winehq.repo
 sudo dnf install winehq-stable
+
+# Verify your NVIDIA Drivers are working 
+lsmod | grep -i nvidia
+
 sudo snap install --classic code # or code-insiders #Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
