4166e8b580041bc3922317460455252fe0033701
Joe <mahinthjoe@gmail.com>
2024-04-27 20:39:41 +0530

    edit sudo in the script disableGUIreboot.sh

--- fedora/disableGUIreboot.sh
+++ fedora/disableGUIreboot.sh
@@ -1,5 +1,5 @@
 # Manual NVIDIA Driver Install Disabling Nouveau Drivers in Fedora
-echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 # regenerate the initramfs image with the new blacklisting configuration to prevent the Nouveau driver from loading
 sudo dracut --force
 # Fedora booting into a text-based, multi-user mode instead of a graphical user interface (GUI).
