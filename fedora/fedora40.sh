0380891c64abd7bdded3acd71003272c9d0ddfa8
Joe <mahinthjoe@gmail.com>
2024-05-11 08:28:48 +0530

    added sudo to command

--- fedora/fedora40.sh
+++ fedora/fedora40.sh
@@ -41,7 +41,7 @@ sudo dnf install openssl
 # Preparing the Public/Private Keys
 cd $home 
 mkdir -p bin/drivers && cd bin/drivers
-openssl req -new -x509 -newkey rsa:2048 -keyout /home/$USER/bin/drivers/Nvidia.key -outform DER -out /home/$USER/bin/drivers/Nvidia.der -nodes -days 100000 -subj "/CN=Graphics Drivers"
+sudo openssl req -new -x509 -newkey rsa:2048 -keyout /home/$USER/bin/drivers/Nvidia.key -outform DER -out /home/$USER/bin/drivers/Nvidia.der -nodes -days 100000 -subj "/CN=Graphics Drivers"
 $ sudo mokutil --import /home/$USER/bin/drivers/Nvidia.der
 #As mentioned in link1 by itpropmn07:
 
