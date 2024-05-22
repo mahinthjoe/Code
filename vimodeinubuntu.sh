4521bedd13073fa0d8bbdd4b638ede3c7ea88383
Joe <Mahinth@FC40-MS7E06>
2024-05-22 14:27:55 +0530

    Easy way to fix Insert mode missing in vi or ubuntu

---
+++ vimodeinubuntu.sh
@@ -0,0 +1,6 @@
+#Cannot Enter "Insert" mode in VI (not VIM)
+#link https://superuser.com/questions/493554/cannot-enter-insert-mode-in-vi-not-vim
+# The way I fixed these problems was by installing these packages.
+sudo apt-get install vim-gui-common
+sudo apt-get install vim-runtime
+ 
