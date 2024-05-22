7ca64b9de09afcf899435851a5f740b58561bfcd
Joe <Mahinth@FC40-MS7E06>
2024-05-22 14:24:48 +0530

    Added Javascript loop speed edit

--- node/speed-loop.js
+++ node/speed-loop.js
@@ -1,4 +1,7 @@
-# JavaScript looped 100000000 times in 0.135 seconds in node v16.13.2
+/* JavaScript looped 100000000 times in 0.135 seconds in node v16.13.2 
+ * JavaScript looped 100000000 times in 0.112 seconds in node v20.12.2
+ * JavaScript looped 100000000 times in 0.036 seconds in node v20.13.1
+ */
 function main() {
   const cycles = 100000000;
   let start = Date.now();
@@ -7,6 +10,6 @@ function main() {
 	}
   let end = Date.now();
   let duration = (end - start) / 1000;
-  console.log("JavaScript looped %d times in %d seconds", cycles, duration);
+  console.log(" looped %d times in %d seconds", cycles, duration);
   }
 main();
