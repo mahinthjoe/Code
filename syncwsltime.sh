70ed3dfdff135c4bd9b0d24284c1b4792083597b
Joe <mahinthjoe@gmail.com>
2024-12-17 12:37:47 +0530

    Add files via upload
    Script added to sync timezone in wsl ubuntu linuix

---
+++ syncwsltime.sh
@@ -0,0 +1,5 @@
+❯ sudo dpkg-reconfigure tzdata
+
+#Current default time zone: 'Asia/Kolkata'
+#Local time is now:      Tue Dec 17 08:48:28 IST 2024.
+#Universal Time is now:  Tue Dec 17 03:18:28 UTC 2024.
